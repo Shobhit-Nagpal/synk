e4661dfd3ed052b3adbdc46164acb0a5081f2413
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-02-09 10:26:48 +0530

    Tried bun shell

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import synk from "./synk";
+import { $ } from "bun";
 
 synk.log("Test log");
 synk.createFile("test.txt");
 synk.write("Test text\n", "test.txt");
 synk.write("Test text\n", "new.txt");
 synk.pwd();
+
+await $`cat test.txt`;
